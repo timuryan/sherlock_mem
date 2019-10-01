2e8e7c981037988ecdd1bd44e22feecac335822b
Putin <timur@wgames.com>
2019-10-01 12:42:23 -0400

    updated v ersion

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SherlockMem.MixProject do
   def project do
     [
       app: :sherlock_mem,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
